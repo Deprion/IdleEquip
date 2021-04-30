4f7d5acbfc69756ae4401b6ec3084cdf4c2088ed
Depri0n <Depri0n@yandex.ru>
2021-04-30 20:16:54 +0300

    updating dll

--- Assets/Scripts/Managers/EventManager.cs
+++ Assets/Scripts/Managers/EventManager.cs
@@ -4,4 +4,15 @@ public class EventManager : MonoBehaviour
 {
     public delegate void IsAvailableUpgradeDel();
     public event IsAvailableUpgradeDel IsAvailableUpgrade;
+    public delegate void ExpGainDel(long amount);
+    public event ExpGainDel ExpGain;
+    private void Start()
+    {
+
+        ExpGain += AddExp();
+    }
+    private void AddExp(long amount)
+    { 
+        
+    }
 }
